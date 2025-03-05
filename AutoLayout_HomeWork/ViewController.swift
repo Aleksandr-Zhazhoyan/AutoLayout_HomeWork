afe6dac2f7805e32bee1d8f2ee8f89c968689c7e
Aleksandr-Zhazhoyan <zhazhoyan85@gmail.com>
2025-03-06 02:08:25 +0400

    HomeWork_14-11-2024

--- AutoLayout_HomeWork/ViewController.swift
+++ AutoLayout_HomeWork/ViewController.swift
@@ -8,12 +8,98 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private let firstLabel = UILabel()
+    private let secondLabel = UILabel()
+    private let imageView = UIImageView()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupUI()
+    }
+    
+    private func setupUI() {
+        
+        view.backgroundColor = .white
+        
+        firstLabel.text = "First Label!"
+        firstLabel.textAlignment = .center
+        firstLabel.backgroundColor = .red
+        
+        firstLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(firstLabel)
+        
+        
+        secondLabel.text = "Second Label!"
+        secondLabel.textAlignment = .center
+        secondLabel.backgroundColor = .purple
+        
+        secondLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(secondLabel)
+        
+        
+        imageView.image = UIImage(systemName: "formfitting.gamecontroller")
+        imageView.contentMode = .scaleAspectFit
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(imageView)
+        
+        
+        NSLayoutConstraint.activate([
+            
+            firstLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80),
+            firstLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            firstLabel.widthAnchor.constraint(lessThanOrEqualTo: view.widthAnchor, multiplier: 0.4),
+            firstLabel.heightAnchor.constraint(equalToConstant: 50),
+            
+            secondLabel.topAnchor.constraint(equalTo: firstLabel.bottomAnchor, constant: 20),
+            secondLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            secondLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            secondLabel.heightAnchor.constraint(equalToConstant: 50),
+            
+            imageView.bottomAnchor.constraint(equalTo: secondLabel.topAnchor, constant: -10),
+            imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            imageView.widthAnchor.constraint(equalToConstant: 100),
+            imageView.heightAnchor.constraint(equalToConstant: 100)
+        ])
+        
+    }
+    
+    override func viewWillLayoutSubviews() {
+        if UIDevice.current.orientation.isLandscape {
+            layoutSubviewsForLandscape()
+        } else {
+            layoutSubviewsForPortrait()
+        }
+    }
+    
+    private func layoutSubviewsForPortrait() {
+        firstLabel.isHidden = false
+        secondLabel.isHidden = false
+        imageView.isHidden = false
+    }
+    
+    private func layoutSubviewsForLandscape() {
+        firstLabel.isHidden = false
+        secondLabel.isHidden = false
+        imageView.isHidden = false
+        
+        
+        NSLayoutConstraint.activate([
+            firstLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            firstLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            
+            secondLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            secondLabel.topAnchor.constraint(equalTo: firstLabel.topAnchor),
+            
+            imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            imageView.topAnchor.constraint(equalTo: firstLabel.topAnchor)
+        ])
     }
 
 
 }
+
 
